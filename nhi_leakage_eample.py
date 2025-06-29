9ed008872a9b6b350bf603ce3c91312699bea6a8
he-test5 <shemeshpet5@gmail.com>
2025-06-29 17:24:46 +0300

    Update nhi_leakage_eample.py

--- nhi_leakage_eample.py
+++ nhi_leakage_eample.py
@@ -1,3 +1,3 @@
 # This is a fake GitHub token for testing detection only
 
-fake_token = "github_pat_11ACLVBYY024yXOAQGlq8K_TkB4CKHp9lsJmfr7gyqRbpGlsmEgvefREMNWbPaujB5E6N4ABZ770U5LX"
+GITHUB_TOKEN = "github_pat_11ACLVBYY024yXOAQGlq8K_TkB4CKHp9lsJmfr7gyqRbpGlsmEgvefREMNWbPaujB5E6N4ABZ770U5LX"
